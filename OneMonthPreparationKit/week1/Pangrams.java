18ddb4fe0557ed3bdfd4b9629b1aced7aae9e9d7
burakarikboga <barikboga42@gmail.com>
2022-05-31 17:52:20 +0300

    Pangrams.java has been modified with using list of Strings

--- OneMonthPreparationKit/week1/Pangrams.java
+++ OneMonthPreparationKit/week1/Pangrams.java
@@ -1,7 +1,11 @@
 package week1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pangrams {
-    public static String pangrams(String s) {
+
+    public static boolean pangrams(String s) {
         int i = 0;
         char c = 'a';
         s = s.replaceAll(" ", "").toLowerCase();
@@ -9,6 +13,23 @@ public class Pangrams {
             i++;
             c++;
         }
-        return i == 26 ? "pangram" : "not pangram";
+        return i == 26;
+    }
+
+    public static String isPangram(List<String> list) {
+        String result = "";
+        for (String each : list) {
+            result += pangrams(each) ? "1" : "0";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("We promptly judged antique ivory buckles for the prize");
+        list.add("We promptly judged antique ivory buckles for the next prize");
+        list.add("abcdefghijklmnoprstuvyzqxw");
+
+        System.out.println(isPangram(list));
     }
 }
